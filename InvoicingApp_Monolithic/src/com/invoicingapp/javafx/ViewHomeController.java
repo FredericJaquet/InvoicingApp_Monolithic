7066057731265c3d49f02df7cea1fafd19203adf
FredericJaquet <frederic.jaquet@gmail.com>
2024-06-20 10:58:47 +0200

    update HomeView

--- InvoicingApp_Monolithic/src/com/invoicingapp/javafx/ViewHomeController.java
+++ InvoicingApp_Monolithic/src/com/invoicingapp/javafx/ViewHomeController.java
@@ -4,19 +4,17 @@
  */
 package com.invoicingapp.javafx;
 
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.GridPane;
 
 /**
  * FXML Controller class
@@ -25,12 +23,71 @@ import javafx.scene.paint.Color;
  */
 public class ViewHomeController implements Initializable {
 
+    private int imgSize=80;
+    
+    @FXML private GridPane paneCentral;
+    @FXML private Button btnCreateCustomer,btnCreateProvider,btnCreateOrder,
+            btnCreateInvoiceCustomer,btnCreateInvoiceProvider,btnCreateQuote, 
+            btnCreatePo,btnReportIncomes,btnReportOutcomes,
+            btnReportPendings,btnGrafIncomes,btnGrafOutcomes;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        paneCentral.setAlignment(Pos.CENTER);
+        InputStream inputStream;
+        Image img;
+        
+        inputStream=getClass().getResourceAsStream("../img/customers.png");
+        img=new Image(inputStream, imgSize, imgSize, true, true);
+        btnCreateCustomer.setGraphic(new ImageView(img));
+        
+        inputStream=getClass().getResourceAsStream("../img/providers.png");
+        img=new Image(inputStream, imgSize, imgSize, true, true);        
+        btnCreateProvider.setGraphic(new ImageView(img));
+        
+        inputStream=getClass().getResourceAsStream("../img/orders.png");
+        img=new Image(inputStream, imgSize, imgSize, true, true);
+        btnCreateOrder.setGraphic(new ImageView(img));
+        
+        inputStream=getClass().getResourceAsStream("../img/invoiceCustomers.png");
+        img=new Image(inputStream, imgSize, imgSize, true, true);
+        btnCreateInvoiceCustomer.setGraphic(new ImageView(img));
+        
+        inputStream=getClass().getResourceAsStream("../img/invoiceProviders.png");
+        img=new Image(inputStream, imgSize, imgSize, true, true);
+        btnCreateInvoiceProvider.setGraphic(new ImageView(img));
+        
+        inputStream=getClass().getResourceAsStream("../img/quotes.png");
+        img=new Image(inputStream, imgSize, imgSize, true, true);
+        btnCreateQuote.setGraphic(new ImageView(img));
+        
+        inputStream=getClass().getResourceAsStream("../img/po.png");
+        img=new Image(inputStream, imgSize, imgSize, true, true);
+        btnCreatePo.setGraphic(new ImageView(img));
+        
+        inputStream=getClass().getResourceAsStream("../img/reportIncomes.png");
+        img=new Image(inputStream, imgSize, imgSize, true, true);
+        btnReportIncomes.setGraphic(new ImageView(img));
+        
+        inputStream=getClass().getResourceAsStream("../img/reportOutcomes.png");
+        img=new Image(inputStream, imgSize, imgSize, true, true);
+        btnReportOutcomes.setGraphic(new ImageView(img));
+        
+        inputStream=getClass().getResourceAsStream("../img/reportPendings.png");
+        img=new Image(inputStream, imgSize, imgSize, true, true);
+        btnReportPendings.setGraphic(new ImageView(img));
+        
+        inputStream=getClass().getResourceAsStream("../img/grafIncomes.png");
+        img=new Image(inputStream, imgSize, imgSize, true, true);
+        btnGrafIncomes.setGraphic(new ImageView(img));
+        
+        inputStream=getClass().getResourceAsStream("../img/grafOutcomes.png");
+        img=new Image(inputStream, imgSize, imgSize, true, true);
+        btnGrafOutcomes.setGraphic(new ImageView(img));
+        
     }
 
     @FXML protected void onFocus(MouseEvent e){
